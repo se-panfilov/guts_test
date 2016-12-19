6dbefe950134d42aa38539a1475876a10b1e2e55
Sergey Panfilov <se-panfilov@ya.ru>
2016-12-19 15:29:36 +0100

    added main header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <main-header></main-header>
+
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import MainHeader from './components/main-header.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    MainHeader
   }
 }
 </script>
---
+++ src/components/main-header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <h1 class="header__title">Guts</h1>
+    <ul class="header__menu--left">
+      <li>
+        <input type="search"
+               v-model="search"
+        />
+      </li>
+    </ul>
+    <ul class="header__menu--right header-menu">
+      <li>
+        <button type="button" class="header-menu__button">Most popular</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MainHeader',
+    data () {
+      return {
+        search: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
