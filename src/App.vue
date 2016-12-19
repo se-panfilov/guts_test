7df50bc6231c5fd906318aee93104a0eb937a764
Sergey Panfilov <se-panfilov@ya.ru>
2016-12-19 16:24:55 +0100

    addedleft menu

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
   <div id="app">
     <main-header></main-header>
     <games-list></games-list>
-
+    <left-menu></left-menu>
   </div>
 </template>
 
 <script>
 import MainHeader from './components/main-header'
+import LeftMenu from './components/left-menu'
 import GamesList from './components/games-list'
 
 export default {
   name: 'app',
   components: {
     MainHeader,
+    LeftMenu,
     GamesList
   }
 }
---
+++ src/components/left-menu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="left-menu">
+    <ul>
+      <li v-for="t in types">
+      <button type="button" v-text="t"></button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import Games from './games'
+
+  export default {
+    name: 'LeftMenu',
+    data () {
+      return {
+        types: []
+      }
+    },
+    mounted () {
+      this.types = this.getTypes(Games)
+      console.info(this.types)
+    },
+    methods: {
+      getTypes (gamesArr) {
+        return gamesArr
+          .map(v => {
+            return v.gameType
+          })
+          .filter((item, i, arr) => {
+            return arr.indexOf(item) === i
+          })
+      }
+    },
+    components: {
+      Games
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
